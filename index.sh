8cf1f82ba5ad209c538579d0eb8ca850677e8866
Aman Bhardwaj <devproddeploy@gmail.com>
2020-10-31 21:24:07 +0530

    init

---
+++ index.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# echo 'hello'
+
+# LANG="JavaScript"
+# echo "I code $LANG"
+# echo "I code ${LANG}"
+
+# read -p "Enter your poison: " POISON
+# echo "your poison is ${POISON}"
+
+NAME="A-Man"
+if [ "$Name" == "A-Man" ]
+then 
+echo "A-Man"
+elif [ "$Name" == "X" ]
+then
+echo "X"
+else
+echo "None"
+fi
